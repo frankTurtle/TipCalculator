e475cd568bfad55437083d5c833433377abf7469
Barret J. Nobel <bear.nobel@gmail.com>
2016-05-26 22:23:20 -0400

    added UI elements hooked up

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalWithTipLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+    }
 
+    @IBAction func implicitlyConvertButton(sender: UIButton) {
+    }
 }
 
